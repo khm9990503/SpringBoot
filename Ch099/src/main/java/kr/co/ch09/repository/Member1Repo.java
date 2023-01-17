ed9f1e690794b436fc7af5fd6c7dbd1eb8102753
khm9990503 <khm9990503@gmail.com>
2023-01-17 11:38:14 +0900

    del

--- Ch099/src/main/java/kr/co/ch09/repository/Member1Repo.java
+++
@@ -1,9 +0,0 @@
-package kr.co.ch09.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.co.ch09.vo.MemberVO;
-
-public interface Member1Repo extends JpaRepository<MemberVO, String> {
-
-}
