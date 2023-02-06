cd888d683dd1a706f23a31741a9808c10263ff83
khm9990503 <khm9990503@gmail.com>
2023-02-06 13:56:06 +0900

    20223/02/06

--- farmstory/src/main/java/kr/co/farmstory/controller/MainController.java
+++ farmstory/src/main/java/kr/co/farmstory/controller/MainController.java
@@ -1,9 +1,13 @@
 package kr.co.farmstory.controller;
 
 import kr.co.farmstory.service.ArticleService;
+import kr.co.farmstory.vo.ArticleVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -13,9 +17,15 @@ public class MainController {
     private ArticleService service;
 
     @GetMapping(value={"","index"})
-    public String index(){
+    public String index(Model model){
 
+        List<ArticleVO> grows = service.selectArticlesIDX("grow");
+        List<ArticleVO> schools = service.selectArticlesIDX("school");
+        List<ArticleVO> stories = service.selectArticlesIDX("story");
 
+        model.addAttribute("grows",grows);
+        model.addAttribute("schools",schools);
+        model.addAttribute("stories",stories);
 
         return "index";
     }
